b6bae54aff3e7fd85847bc577207375944a4d3d6
Jonathan Tapnio <jonathan.tapnio@cambridge.org>
2025-12-18 15:03:09 +0800

    Revert "Add user claims endpoint and update authorization scopes"
    This reverts commit 1efa1df237eb185bc4e65df8a57f8cfd32c053e2.

--- controller/authController.js
+++ controller/authController.js
@@ -7,7 +7,7 @@ const getCodeVerifier = () => {
 const getAuthorizationUrl = (client, oidcIssuer, codeVerifier) => {
   const codeChallenge = generators.codeChallenge(codeVerifier);
   return client.authorizationUrl({
-    scope: "openid profile picture email uid roles orgs CI_open_closed_status CI_operational_status TC_operational_status CI_product_eligibilities TC_product_eligibilities",
+    scope: "openid email profile orgs roles uid",
     resource: oidcIssuer.metadata.authorization_endpoint,
     code_challenge: codeChallenge,
     code_challenge_method: "S256",
